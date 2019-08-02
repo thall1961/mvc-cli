64e6c2a16046fec6f69270d18fad9cb2b4830715
thall1961 <thall1961@gmail.com>
2019-08-02 15:00:30 -0600

    initial commit

---
+++ src/cli.js
@@ -0,0 +1,69 @@
+import arg from 'arg';
+import inquirer from 'inquirer';
+import {createProject} from './main';
+
+function parseArgumentsIntoOptions(rawArgs) {
+  const args = arg(
+    {
+      '--git': Boolean,
+      '--yes': Boolean,
+      '--install': Boolean,
+      '-g': '--git',
+      '-y': '--yes',
+      '-i': '--install'
+    },
+    {
+      argv: rawArgs.slice(2)
+    }
+  );
+
+  return {
+    skipPrompts: args['--yes'] || false,
+    git: args['--git'] || false,
+    template: args._[0],
+    runInstall: args['--install'] || false
+  };
+}
+
+async function promptForMissingOptions(options) {
+  const defaultTemplate = 'JavaScript';
+  if (options.skipPrompts) {
+    return {
+      ...options,
+      template: options.template || defaultTemplate
+    };
+  }
+
+  const questions = [];
+  if (!options.template) {
+    questions.push({
+      type: 'list',
+      name: 'template',
+      message: 'Which template do you want to use?',
+      choices: ['JavaScript', 'TypeScript'],
+      default: defaultTemplate
+    });
+  }
+
+  if (!options.git) {
+    questions.push({
+      type: 'confirm',
+      name: 'git',
+      message: 'Initialize a git repository?',
+      default: false
+    });
+  }
+
+  const answer = await inquirer.prompt(questions);
+  return {
+    ...options,
+    template: options.template || answer.template,
+    git: options.git || answer.git
+  };
+}
+
+export async function cli(args) {
+  let options = parseArgumentsIntoOptions(args);
+  options = await promptForMissingOptions(options);
+  await createProject(options);
+}
